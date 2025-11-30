5dbd7f86005fcc21d606a960d7caa4628bc938f8
Daniel Abreu <daniel.frioli@gmail.com>
2025-11-29 22:24:02 -0300

    Adding Password Attempt B Case, Winning Condition and Improvements in Timer

--- Quartus_II_Project/Segment_Display_7.v
+++ Quartus_II_Project/Segment_Display_7.v
@@ -1,23 +1,23 @@
 module Segment_Display_7(
     input [3:0] valor , // Número 0 -> 9
-    output reg [6:0]hex // (a,b,c,d,e,g,h)
+    output reg [7:0]hex // (dot,a,b,c,d,e,f,g)
 );
 
     always @(*) begin
         case (valor)
 
-           4'd0: hex = 7'b1000000; // 0
-           4'd1: hex = 7'b1111001; // 1
-           4'd2: hex = 7'b0100100; // 2
-           4'd3: hex = 7'b0110000; // 3
-           4'd4: hex = 7'b0011001; // 4
-           4'd5: hex = 7'b0010010; // 5
-           4'd6: hex = 7'b0000010; // 6
-           4'd7: hex = 7'b1111000; // 7
-           4'd8: hex = 7'b0000000; // 8
-           4'd9: hex = 7'b0010000; // 9
+            4'd0: hex = 8'b10000001; // 0
+            4'd1: hex = 8'b11110011; // 1
+            4'd2: hex = 8'b01001001; // 2
+            4'd3: hex = 8'b01100001; // 3
+            4'd4: hex = 8'b00110011; // 4
+            4'd5: hex = 8'b00100101; // 5
+            4'd6: hex = 8'b00000101; // 6
+            4'd7: hex = 8'b11110001; // 7
+            4'd8: hex = 8'b00000001; // 8
+            4'd9: hex = 8'b00100001; // 9
 
-           default: hex = 7'b1111111;
+            default: hex = 8'b11111111;
         endcase
     end
 endmodule
--- Quartus_II_Project/Time_Bomb.v
+++ Quartus_II_Project/Time_Bomb.v
@@ -1,15 +1,19 @@
 module Time_Bomb (
     input CLOCK_50,
     input [3:0] KEY,
-	 input [17:11] SW,
-    output [6:0] HEX3,
-    output [6:0] HEX2,
-    output [6:0] HEX1,
-    output [6:0] HEX0
+	input [17:0] SW,
+    output [7:0] LEDG,
+    output [7:0] HEX4,
+    output [7:0] HEX3,
+    output [7:0] HEX2,
+    output [7:0] HEX1,
+    output [7:0] HEX0
 );
     wire [3:0] Wire_A_Pin;
     wire [2:0] Wire_B_Pin;
+    wire [2:0] Wire_Attempt_B;
     wire Wire_Done_Register;
+    wire Wire_Game_Won;
 
     wire fio_tick_1s;
     wire [3:0] fio_min_u;
@@ -33,18 +37,32 @@ module Time_Bomb (
         .Done_Register(Wire_Done_Register)
     );
 
+    Attempt_B INST_ATTEMPT_B (
+        .Clk(CLOCK_50),
+        .Start(), // Precisa de Sinal de Confirmação da Saída de Tentativa A.
+        .Enter(~KEY[3]),
+        .Attempt_State(), // Precisa de Sinal de Confirmação do Comparador.
+        .Switches_Attempt(SW[2:0]),
+        .Attempt(Wire_Attempt_B),
+        .Display(HEX4),
+        .Game_Won(Wire_Game_Won)
+    );
+
     // Instância do Cronômetro
     Cronometer INST_CRONOMETRO (
         .clk(CLOCK_50),
         .reset(~KEY[1]),
         .start(Wire_Done_Register),
-        .game_won(1'b0),
+        .game_won(Game_Won),
         .tick_1s(fio_tick_1s),
         .min_unidade(fio_min_u),
         .seg_dezena(fio_seg_d),
         .seg_unidade(fio_seg_u),
         .time_over() 
     );
+
+    // Caso de Vitória
+    assign LEDG = Game_Won;
 
     // Displays
     Segment_Display_7 DISP_MIN (
@@ -62,6 +80,6 @@ module Time_Bomb (
         .hex(HEX1)
     );
 
-    assign HEX0 = 7'b1111111;
+    assign HEX0 = 8'b11111111;
 
 endmodule
